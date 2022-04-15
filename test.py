2e60e0ebc2021de39da755ff0a1354671b158180
Phanzsx <975086188@qq.com>
2022-04-15 23:05:40 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a = (0.1112212112, 0.2)
-s = ['1', '2']
-c = (a, s)
-with open('log.txt', 'w') as f:
-    for i in a:
-        f.write(str(round(i, 2)) + ' ')
-    f.write('\n')
